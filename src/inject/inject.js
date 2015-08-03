cbcd01ba33d3d8b4408d97ddbe3aa80eb22984dc
Mike Kenyon <mike.kenyon@gmail.com>
2015-08-02 21:14:09 -0400

    Set color on all newly-added labels

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,14 +1,38 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-		if (document.readyState === "complete") {
-			clearInterval(readyStateCheckInterval);
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-			var labels = document.getElementsByClassName('label');
-			Array.prototype.filter.call(labels, function(label) {
-				return label.textContent.indexOf('blocked') === 0;
-			}).forEach(function(label) {
-				label.style.color = 'rgb(199, 37, 67)';
-			});
-		}
-	}, 4000);
+      var colorMapping = {
+        'blocked': 'rgb(199, 37, 67)',
+        'needs ': 'rgb(199, 37, 67)'
+      };
+
+      function colorLabelNodes(labels) {
+        Object.keys(colorMapping).forEach(function(labelKeyword) {
+          Array.prototype.filter.call(labels, function(label) {
+            return label.textContent.indexOf(labelKeyword) === 0
+          }).forEach(function(label) {
+            label.style.color = colorMapping[labelKeyword];
+          });
+        });
+      }
+
+      var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+          Array.prototype.forEach.call(mutation.addedNodes, function(addedNode) {
+            if (typeof addedNode.getElementsByClassName !== 'undefined') {
+              var labels = addedNode.getElementsByClassName('label');
+              colorLabelNodes(labels);
+            }
+          });
+        });
+      });
+
+      // configuration of the observer:
+      var config = { childList: true, subtree: true };
+
+      observer.observe(document, config);
+    }
+  }, 10);
 });
