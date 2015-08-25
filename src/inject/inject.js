64fa6a139fa0f6ff36b31b90500c025b8a59a972
Mike Kenyon <mike.kenyon@gmail.com>
2015-08-24 22:03:25 -0400

    Observe mutations of attributes, characterData
    Read the manual. Turns out that the MutationObserver API allows me to
    track _lots_ of things. React's algorithm will sometimes add elements,
    sometimes remove them, and sometimes simply modify the existing ones.
    By capturing `attributes` and `characterData` in addition to the
    `childList` and `subtree`, I think that I can finally collect all of
    the information needed.
    
    Also removing embarrassing interval. It was a quick hack, but gross.
    
    Finishes #3
    Fixes #6

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,15 +7,6 @@ chrome.extension.sendMessage({}, function(response) {
         Array.prototype.forEach.call(labels, function(label) {
           if (isLabelEligible(label.textContent)) {
             label.classList.add('blocked');
-
-            // Handle wrapping in conjunction with white-space: nowrap;
-            label.insertAdjacentHTML('afterend', '<span> </span>');
-
-            // Handle spacing after comma
-            label.textContent = label.textContent.trim();
-            if (label.textContent.charAt(label.textContent.length - 1) === ',') {
-              label.style.paddingRight = '5px';
-            }
           } else {
             label.classList.remove('blocked');
           }
@@ -37,38 +28,22 @@ chrome.extension.sendMessage({}, function(response) {
         }
       }
 
-      var overrideTimer = 3000;
-      function override() {
-        var labels = document.getElementsByClassName('label');
-
-        var t0 = performance.now();
-        colorLabelNodes(labels);
-        var t1 = performance.now();
-        var duration = t1 - t0;
-
-        if (duration > (overrideTimer * 0.1)) {
-          overrideTimer = overrideTimer * 4;
-        } else {
-          overrideTimer = overrideTimer / 1.5;
-        }
-
-        setTimeout(override, overrideTimer);
-      }
-
       var observer = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
           Array.prototype.forEach.call(mutation.addedNodes, colorLabelsInNode);
+          colorLabelsInNode(mutation.target);
         });
       });
 
       // configuration of the observer:
-      var config = { childList: true, subtree: true };
+      var config = {
+        attributes: true,
+        characterData: true,
+        childList: true,
+        subtree: true
+      };
 
       observer.observe(document, config);
-
-      colorLabelsInNode(document);
-
-      setTimeout(override, overrideTimer);
     }
   }, 10);
 });
