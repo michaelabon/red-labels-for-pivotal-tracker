91ac01fc30df73fa940b4a28c7c1e3c459eee528
Carlos J. Argüello <cjarguello@users.noreply.github.com>
2017-04-30 16:45:45 -0700

    Update and rename isLabelEligible.js to isLabelRed.js

--- src/inject/isLabelRed.js
+++ src/inject/isLabelRed.js
@@ -1,7 +1,3 @@
 var isLabelRed = function isLabelRed(labelText) {
   return !!labelText.match(/\b(?:blocked|needs)\b/);
 };
-
-var isLabelBlue = function isLabelBlue(labelText) {
-  return !!labelText.match(/\b(?:planner\b/);
-};
