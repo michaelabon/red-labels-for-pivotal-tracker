7e072903cf2c265fa81e0f0016914ff9ae31041a
Carlos J. Argüello <cjarguello@users.noreply.github.com>
2017-04-30 16:24:37 -0700

    Update isLabelEligible.js

--- src/inject/isLabelEligible.js
+++ src/inject/isLabelEligible.js
@@ -1,3 +1,7 @@
-var isLabelEligible = function isLabelEligible(labelText) {
+var isLabelRed = function isLabelRed(labelText) {
   return !!labelText.match(/\b(?:blocked|needs)\b/);
+};
+
+var isLabelBlue = function isLabelBlue(labelText) {
+  return !!labelText.match(/\b(?:planner\b/);
 };
