5f470ad9034057c23ca5e0895d3bb7768f10e0d2
Carlos J. Argüello <cjarguello@users.noreply.github.com>
2017-04-30 16:30:01 -0700

    Update isLabelEligibleSpec.js

--- test/isLabelEligibleSpec.js
+++ test/isLabelEligibleSpec.js
@@ -1,41 +1,41 @@
-describe('isLabelEligible', function () {
+describe('isLabelRed', function () {
   describe('approves of', function () {
     it('blocked', function () {
-      expect(isLabelEligible('blocked')).toEqual(true);
+      expect(isLabelRed('blocked')).toEqual(true);
     });
 
     it('needs design', function () {
-      expect(isLabelEligible('needs design')).toEqual(true);
+      expect(isLabelRed('needs design')).toEqual(true);
     });
 
     it('needs detail', function () {
-      expect(isLabelEligible('needs detail')).toEqual(true);
+      expect(isLabelRed('needs detail')).toEqual(true);
     });
 
     it('needs-pm', function () {
-      expect(isLabelEligible('needs-pm')).toEqual(true);
+      expect(isLabelRed('needs-pm')).toEqual(true);
     });
 
     it('▩▩blocked▩▩', function () {
-      expect(isLabelEligible('▩▩blocked▩▩')).toEqual(true);
+      expect(isLabelRed('▩▩blocked▩▩')).toEqual(true);
     });
 
     it('▩▩ blocked ▩▩', function () {
-      expect(isLabelEligible('▩▩ blocked ▩▩')).toEqual(true);
+      expect(isLabelRed('▩▩ blocked ▩▩')).toEqual(true);
     });
 
     it('✖ needs backend story ✖', function () {
-      expect(isLabelEligible('✖ needs backend story ✖')).toEqual(true);
+      expect(isLabelRed('✖ needs backend story ✖')).toEqual(true);
     });
 
     it('-blocked-', function () {
-      expect(isLabelEligible('-blocked-')).toEqual(true);
+      expect(isLabelRed('-blocked-')).toEqual(true);
     });
   });
 
   describe('hates', function () {
     it('need supply', function () {
-      expect(isLabelEligible('need supply')).toEqual(false);
+      expect(isLabelRed('need supply')).toEqual(false);
     });
   });
 });
